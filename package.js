5006292773f9509301cee4a5497a4168cbe5f466
santosportomatheus54-create <santosportomatheus54@gmail.com>
2026-02-13 09:41:25 -0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "bot-premium-org",
+  "version": "2.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "dotenv": "^16.4.5"
+  }
+}
